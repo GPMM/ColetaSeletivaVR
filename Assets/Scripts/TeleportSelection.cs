0755a7834ddb5564eaa850a8488c7601f8c11b22
Gabriel de Souza <devgabrieldesouza@gmail.com>
2020-11-07 09:37:48 -0300

    Teleport and Grabbable Test Object
    - Added and Fixed Teleport System with gameobjects responsive to player interactions.
    - Added a coin as a physical gameobject and the player can grab it.

---
+++ Assets/Scripts/TeleportSelection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportSelection : MonoBehaviour
+{
+
+	[SerializeField]
+    private bool isRotating = false; 
+
+    [SerializeField]
+    private GameObject[] activeWhenSelect;
+
+	[SerializeField]
+	private GameObject[] rotateWhenSelect;
+
+	[SerializeField]
+	private float[] speed;
+
+    public void SelectTarget(){
+        foreach(GameObject part in activeWhenSelect){
+			part.SetActive(true);
+        }
+
+        isRotating = true;
+    }
+
+    public void DeselectTarget(){
+        foreach(GameObject part in activeWhenSelect){
+			part.SetActive(false);
+		}
+
+		isRotating = false;
+    }
+
+    private void Update() {
+        if(isRotating){
+            for(int i = 0; i < rotateWhenSelect.Length; i++){
+                rotateWhenSelect[i].transform.Rotate(Vector3.up * speed[i] * Time.deltaTime, Space.World);
+            }
+        }
+    }
+
+}
